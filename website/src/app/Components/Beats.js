3ab684bb8729668c976703bd7d565f5a65ba939d
mdfaizan973 <faizan.md9735@gmail.com>
2023-08-19 00:37:06 +0530

    able to add EQ beats IQ

---
+++ website/src/app/Components/Beats.js
@@ -0,0 +1,70 @@
+import { Typography, Box, Grid } from "@mui/material";
+export default function Home() {
+  return (
+    <div
+      style={{
+        marginBottom: "24px",
+        width: "90%",
+        margin: "auto",
+        padding: "16px",
+        display: "flex",
+        justifyContent: "space-between",
+        gap: "25px",
+        flexWrap: "wrap",
+      }}
+    >
+      <Box
+        elevation={3}
+        style={{
+          flex: "1",
+          marginBottom: "16px",
+          padding: "20px",
+
+          backgroundColor: "white",
+        }}
+      >
+        <Typography variant="h4" component="div">
+          EQ beats IQ
+        </Typography>
+      </Box>
+
+      <Box
+        elevation={3}
+        style={{
+          flex: "1",
+          marginBottom: "16px",
+          padding: "20px",
+
+          backgroundColor: "white",
+        }}
+      >
+        <Typography variant="h6">
+          People with high emotional <br />
+          intelligence (Eq) live more fulfilled
+          <br />
+          lives. They tend to be happier and
+          <br /> have healthier relationship.
+        </Typography>
+      </Box>
+
+      <Box
+        elevation={3}
+        style={{
+          flex: "1",
+          marginBottom: "16px",
+          padding: "20px",
+
+          backgroundColor: "white",
+        }}
+      >
+        <Typography variant="h6">
+          They are more successful in their
+          <br /> pursuits and make for inspiring
+          <br />
+          leaders. According to since, they
+          <br /> eard $29k a year.
+        </Typography>
+      </Box>
+    </div>
+  );
+}
--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,11 +1,12 @@
 import Navbar from "./Components/Navbar";
 import MasterSection from "./Components/Home";
-
+import Beats from "./Components/Beats";
 export default function Home() {
   return (
     <main>
       <Navbar />
       <MasterSection />
+      <Beats />
     </main>
   );
 }
